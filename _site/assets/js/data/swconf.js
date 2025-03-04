01adb1bd98aabf15190f4ba018ebceb80e481aaa
Petteri Heino <petteriheino@Petteri-MacBook-Pro.local>
2025-03-04 09:19:01 +0200

    chore: sitemap.xml määritys

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1740857849',resources: [
+    cacheName: 'chirpy-1741072710',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
